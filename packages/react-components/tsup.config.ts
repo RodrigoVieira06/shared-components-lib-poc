d4e36df8bc6ac497f5ded1a7371124a84c9539de
RodrigoVieira <rodrigo.vieira@levva.io>
2024-11-21 16:49:24 -0300

    chore: changing pakcage.json configurations

--- packages/react-components/tsup.config.ts
+++ packages/react-components/tsup.config.ts
@@ -1,15 +1,13 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/components/**/*.{ts,tsx}"],
+  entry: [
+    "src/components/**/!(*.stories).{ts,tsx}"
+  ],
   dts: true,
   outDir: "dist",
-  format: ["esm"],
   splitting: false,
   clean: true,
   minify: true,
   target: "ES6",
-  esbuildOptions: (options) => {
-    options.chunkNames = "[name]";
-  },
 });
