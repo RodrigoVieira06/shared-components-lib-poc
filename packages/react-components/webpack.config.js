4cbe1f3b62293266f16431f65f80e6d0951bb280
RodrigoVieira <rodrigo.vieira@levva.io>
2024-11-22 12:40:47 -0300

    chore: webpack adjustments

--- packages/react-components/webpack.config.js
+++ packages/react-components/webpack.config.js
@@ -2,34 +2,32 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    MainButton: './src/MainButton/index.ts', // Entrada para o MainButton
-    MainInput: './src/MainInput/index.ts',   // Entrada para o MainInput
+    MainButton: './src/MainButton/index.ts',
+    MainInput: './src/MainInput/index.ts',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: (pathData) => {
-      // Gera o nome do arquivo baseado no nome da entrada
       return `${pathData.chunk.name}/index.js`;
     },
-    libraryTarget: 'umd', // Cria pacotes UMD que podem ser usados em qualquer lugar
+    libraryTarget: 'umd',
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'], // Extensões de arquivos que o Webpack pode resolver
+    extensions: ['.ts', '.tsx', '.js'],
   },
   module: {
     rules: [
       {
-        test: /\.tsx?$/, // Regra para arquivos TypeScript
-        use: 'ts-loader', // Usa o ts-loader para compilar TypeScript
+        test: /\.tsx?$/,
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
   },
   externals: {
-    // Marque dependências como externas para evitar que sejam bundladas
     react: 'react',
     'react-dom': 'react-dom',
   },
-  mode: 'production', // Modo de produção para otimizar o build
-  devtool: 'source-map', // Geração de sourcemaps
+  mode: 'production',
+  devtool: 'source-map',
 };
