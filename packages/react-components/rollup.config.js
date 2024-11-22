be224939f436f96c381c10670fe0198328150110
RodrigoVieira <rodrigo.vieira@levva.io>
2024-11-22 01:17:12 -0300

    chore: fixing rollup configurations

--- packages/react-components/rollup.config.js
+++ packages/react-components/rollup.config.js
@@ -2,34 +2,24 @@ const path = require('path');
 const typescript = require('rollup-plugin-typescript2');
 
 module.exports = {
+  external: ['react', 'react-dom', 'antd'],
   input: [
-    'src/MainButton/index.ts',  // Arquivo de entrada para Button
-    'src/MainInput/index.ts',   // Arquivo de entrada para Input
-    // Adicione mais componentes conforme necessário
+    'src/MainButton/index.ts',
+    'src/MainInput/index.ts',
   ],
   output: [
     {
-      dir: 'dist',                    // O diretório de saída
-      format: 'esm',                   // Formato ESM para tree shaking
+      dir: 'dist',
+      format: 'esm',
       sourcemap: true,
-      preserveModules: true,           // Mantém os módulos separados
-      preserveModulesRoot: 'src',      // Mantém a estrutura de pastas do src
+      preserveModules: true,
+      preserveModulesRoot: 'src',
     },
-    {
-      dir: 'dist',                    // O diretório de saída
-      format: 'cjs',                   // Formato CommonJS
-      sourcemap: true,
-      preserveModules: true,           // Mantém os módulos separados também para CJS
-      preserveModulesRoot: 'src',      // Mantém a estrutura de pastas do src
-    }
   ],
-  external: ['react', 'react-dom'],
   plugins: [
     typescript({
       tsconfig: path.resolve(__dirname, 'tsconfig.json'),
       useTsconfigDeclarationDir: true,
-    })
+    }),
   ],
-  preserveModules: true,
-  preserveModulesRoot: 'src',
 };
--- packages/react-components/src/MainInput/index.ts
+++ packages/react-components/src/MainInput/index.ts
@@ -1,1 +1,1 @@
-export { default } from "./MainInput";
+export { MainInput } from "./MainInput";
