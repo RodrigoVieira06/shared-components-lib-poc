055a76144532d08cd716844ec4754ac1f57fab71
RodrigoVieira <rodrigo.vieira@levva.io>
2024-11-22 14:15:18 -0300

    chore: rollup config adjustments

--- packages/react-components/rollup.config.js
+++ packages/react-components/rollup.config.js
@@ -1,19 +1,15 @@
-import fs from 'fs';
 import typescript from '@rollup/plugin-typescript';
-import dts from 'rollup-plugin-dts';
 
 export default {
   external: ['react', 'react-dom', 'antd'],
   input: 'src/index.ts',
   output: {
     dir: 'dist',
-    format: 'esm',
-    sourcemap: true,
+    preserveModules: true,
   },
   plugins: [
     typescript({
       tsconfig: './tsconfig.json',
     }),
-    dts(),
   ],
 };
